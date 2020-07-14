551bcb2eb869e459b8cb61b5de115ea62a0519af
hornungt <tthornung@gmail.com>
2020-07-14 18:00:09 -0500

    added manager layer

--- src/controllers/crud.controller.ts
+++ src/controllers/crud.controller.ts
@@ -1,47 +1,34 @@
 import { Application, Router } from "express";
 import { Logger } from "winston";
-import { ObjectID, DeleteWriteOpResultObject } from "mongodb";
 import { MongoRepository } from "../repositories/mongo.repository";
 import ControllerFunction from "./route-controller";
-import { StringDecoder } from "string_decoder";
+import { DemoManager } from "../managers/demo.manager";
 
 // exposes Create, Read, Update, and Delete functionality for a mongodb Atlas repository
 export default ((app: Application, db: MongoRepository, logger: Logger) => {
     const router = Router();
+    const manager = new DemoManager(db, logger);
 
     // create
     router.post("/", async (req, res) => {
-        const op = await db.insert(process.env.DEMO_COLLECTION, req.body);
-        res.send(op.result);
+        const ret = await manager.createOne(req.body);
+        res.send(ret);
     });
 
     // read
     router.get("/", async (req, res) => {
-        let ret: any[];
-        await db.find(process.env.DEMO_COLLECTION, { "val": { $exists: true } })
-            .then(async (val) => ret = await val.toArray())
-            .catch((err) => logger.error(err));
+        const ret = await manager.getAll();
         res.send(ret);
     });
 
     // update
     router.put("/", async (req, res) => {
-        const op = await db.update(process.env.DEMO_COLLECTION,
-            { "_id": new ObjectID(req.body._id) }, { $set: { "val": req.body.val } });
-        res.send(op.result);
+        res.send(await manager.updateByID(req.body.id, req.body.val));
     });
 
     // delete
     router.delete("/", async (req, res) => {
-        // todo: fix this endpoint
-        const ids: ObjectID[] = Array.isArray(req.body.ids) ?
-            req.body.ids.map((id: string) => new ObjectID(id)) : Array.from<ObjectID>(req.body.ids);
-
-        let ret: DeleteWriteOpResultObject;
-        await db.delete(process.env.DEMO_COLLECTION, { _id : { $in: ids }})
-            .then((opResult) => ret = opResult)
-            .catch((err) => logger.error(err));
-        res.send(ret.result);
+        res.send(await manager.deleteByID(req.body.id));
     });
 
     app.use("/crud", router);
---
+++ src/managers/demo.manager.ts
@@ -0,0 +1,41 @@
+import { Logger } from "winston";
+import { MongoRepository } from "../repositories/mongo.repository";
+import { ObjectID, DeleteWriteOpResultObject } from "mongodb";
+
+// manager layer hides direct repository interaction from the controller layer
+export class DemoManager {
+    private repo: MongoRepository;
+    private logger: Logger;
+
+    constructor(repo: MongoRepository, logger: Logger) {
+        this.repo = repo;
+        this.logger = logger;
+    }
+
+    async getAll(): Promise<any[]> {
+        let ret: any[];
+        await this.repo.find(process.env.DEMO_COLLECTION, { "val": { $exists: true } })
+            .then(async (val) => ret = await val.toArray())
+            .catch((err) => this.logger.error(err));
+        return ret;
+    }
+
+    async createOne(obj: any) {
+        return (await this.repo.insert(process.env.DEMO_COLLECTION, obj)).result;
+    }
+
+    async updateByID(id: string, newVal: any) {
+        return (await this.repo.update(process.env.DEMO_COLLECTION,
+            { "_id": new ObjectID(id) }, { $set: { "val": newVal } })).result;
+    }
+
+    async deleteByID(id: string) {
+        let ret: DeleteWriteOpResultObject;
+        await this.repo.delete(process.env.DEMO_COLLECTION, { _id : { $eq: new ObjectID(id) }})
+            .then((opResult) => ret = opResult)
+            .catch((err) => this.logger.error(err));
+
+        return ret.result;
+    }
+
+}
